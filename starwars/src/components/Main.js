354e91ea2ffc68da71bd4ffb756b1cac5999f1ff
Omulosi <mulongojohnpaul@gmail.com>
2019-09-13 12:07:51 +0300

    Add a Main component that composes table Header and table Data components

---
+++ starwars/src/components/Main.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Header from './Header';
+import Data from './Data';
+
+
+const StyledTable = styled.table`
+  border-collapse: separate;
+  border-spacing: 0;
+  color: #4a4a4d;
+  font-size: 0.99em;
+
+  th, td {
+    padding: 10px 15px;
+    vertical-align: middle;
+  }
+
+  .tbody tr {
+  background: #fff;
+  }
+
+
+  thead {
+    background: #395870;
+    background: linear-gradient(#49708f, #293f50);
+    color: #fff;
+    font-size: 11px;
+    text-transform: uppercase;
+  }
+
+  .tbody tr:nth-child(even) {
+    background: #f0f0f2;
+  }
+
+  td, th {
+    border-bottom: 1px solid #cecfd5;
+    border-right: 1px solid #cecfd5;
+  }
+
+  th:first-child {
+    border-top-left-radius: 5px;
+    text-align: left;
+  }
+
+  th:last-child {
+    border-top-right-radius: 5px;
+  }
+
+  .tbody tr:last-child td:first-child {
+    border-bottom-left-radius: 5px;
+  }
+
+  .tbody tr:last-child td:last-child {
+    border-bottom-right-radius: 5px;
+  }
+
+`
+
+const Main = (props) => {
+  const data = props.data;
+
+  console.log(data);
+  return (
+    <StyledTable>
+      <Header/>
+      <tbody className="tbody">
+        {
+          data.map((star, ind) => {
+            return <Data star={star} key={ind}/>
+          })
+        }
+      </tbody>
+    </StyledTable>
+  )
+}
+
+export default Main;
