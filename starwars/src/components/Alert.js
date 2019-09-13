2e1cf4afba2c2b888a2f3a6eacf316775a062202
Omulosi <mulongojohnpaul@gmail.com>
2019-09-13 12:36:17 +0300

    Add an ALert component

---
+++ starwars/src/components/Alert.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const StyledAlert = styled.div`
+  position: absolute;
+  background: rgb(0, 0, 0, 0.3);
+  top: 0;
+  left: 0;
+  z-index: 3;
+  width: 100%;
+  height: 100%;
+
+
+  p {
+    position: relative;
+    top: 20%;
+    font-weight: lighter;
+    color: white;
+    font-size: 3em;
+  }
+`
+
+const Alert = (props) => {
+  const { message } = props;
+
+  return (
+    <StyledAlert>
+      <p>{message}</p>
+    </StyledAlert>
+  )
+}
+
+export default Alert;
