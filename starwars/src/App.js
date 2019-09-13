1bedd3e6f86da4e8fcc0705bdceb1dc6eb8a61df
Omulosi <mulongojohnpaul@gmail.com>
2019-09-13 12:08:45 +0300

    Add Main component to App component with some dummy data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Main from './components/Main';
+
 import './App.css';
 
 const App = () => {
@@ -8,10 +10,50 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  //
+  //
+  const results = [
+    {
+      name: "luke skywalker",
+      height: 172,
+      mass: 77,
+      hair_color: 'blond',
+      skin_color: 'fair',
+      gender: 'male'
+    },
+    {
+      name: "luke skywalker",
+      height: 172,
+      mass: 77,
+      hair_color: 'blond',
+      skin_color: 'fair',
+      gender: 'male'
+    },
+    {
+      name: "luke skywalker",
+      height: 172,
+      mass: 77,
+      hair_color: 'blond',
+      skin_color: 'fair',
+      gender: 'male'
+    },
+    {
+      name: "luke skywalker",
+      height: 172,
+      mass: 77,
+      hair_color: 'blond',
+      skin_color: 'fair',
+      gender: 'male'
+    },
+  ]
 
   return (
     <div className="App container">
       <h1 className="Header">React Wars</h1>
+      <p className='tagline'> Your favorite Star Wars characters!</p>
+      <div className='main-content'>
+        <Main data={results}/>
+      </div>
     </div>
   );
 }
